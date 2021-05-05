b7243b767270ed21009e6c6cff2ac479b82b163c
PuPsIkI17 <vadimpislari98@gmail.com>
2021-05-05 15:08:18 +0100

    added run script

--- run.sh
+++ run.sh
@@ -1,10 +1,2 @@
-## Setup
-1. sudo apt install git-all
-2. git clone https://github.com/PuPsIkI17/RpiTelemetry_Setup
-3. cd RpiTelemetry_Setup/
-4. bash setup.py
-(It may need a restart)
-
-## RUN
-bash run.sh  (or)
 node js/index.js --topic rpi_telemetry --root-ca certs/Amazon-root-CA-1.pem --cert certs/certificate.pem.crt --key certs/private.pem.key --endpoint age8f9ux9o2e3-ats.iot.us-east-1.amazonaws.com
+
