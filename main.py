594cbb2b66528bd9a754f39d3a94d22bd74589cc
PuPsIkI17 <vadimpislari98@gmail.com>
2021-05-05 14:58:02 +0100

    reading basic data from rpi

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import os
 import psutil
 import uuid  
 import platform
-from device_detector import DeviceDetector
+#from device_detector import DeviceDetector
 
 #id
 print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
 for ele in range(0,8*6,8)][::-1]))
 
-#cpu_usage
-load1, load5, load15 = psutil.getloadavg()
-cpu_usage = (load15/os.cpu_count()) * 100
-print(cpu_usage)
+#cpu usage
+print(psutil.cpu_percent(2))
 
-# memory_usage
-total_memory, used_memory, free_memory = map(
-    int, os.popen('free -t -m').readlines()[-1].split()[1:])
-print("RAM memory % used:", round((used_memory/total_memory) * 100, 2))
+memory = psutil.virtual_memory()
+#memory size in gb 
+print(memory.total >> 30)
+#memory usage
+print(memory[2])
+
+
+#print(psutil.net_io_counters(2).bytes_sent + psutil.net_io_counters(2).bytes_recv)
 
 #device info
-my_system = platform.uname()
+#my_system = platform.uname()
  
-print(f"System: {my_system.system}")
-print(f"Node Name: {my_system.node}")
-print(f"Release: {my_system.release}")
-print(f"Version: {my_system.version}")
-print(f"Machine: {my_system.machine}")
-print(f"Processor: {my_system.processor}")
+#print(f"System: {my_system.system}")
+#print(f"Node Name: {my_system.node}")
+#print(f"Release: {my_system.release}")
+#print(f"Version: {my_system.version}")
+#print(f"Machine: {my_system.machine}")
+#print(f"Processor: {my_system.processor}")
